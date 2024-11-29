4f0c1aaad72350f04ffde772c5419dd2c9e0af7d
saika2077 <95666764+saika2077@users.noreply.github.com>
2024-11-29 17:17:21 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,31 +6,24 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u2747502-4eff794a454487be3e9ec949'
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 7,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'ChatGPT Mirror Site',
+      url: 'https://saikachat.pp.ua/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
+      text: 'Get Refresh Token',
+      url: 'https://token.saikachat.pp.ua/'
+    }
   ],
 };
